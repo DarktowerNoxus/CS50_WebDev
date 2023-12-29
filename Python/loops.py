69f117562bb044730634539afed7fb544f942be3
G. Salesch <g.salesch@googlemail.com>
2023-12-29 03:23:56 +0100

    Objects in Python

---
+++ Python/loops.py
@@ -0,0 +1,6 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+# Same as above, but using range function
+for i in range(6):
+    print(i)
---
+++ Python/sets.py
@@ -0,0 +1,10 @@
+#create empty set
+s = set()
+
+# Add elemts to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+
+print(f"The Set has {len(s)} elements")
