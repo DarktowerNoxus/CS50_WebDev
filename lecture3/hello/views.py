d135d40e131e5659ab0b9feb1795c6f00228f087
G. Salesch <g.salesch@googlemail.com>
2023-12-31 04:22:21 +0100

    Auto response to link input

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+def gino(request):
+    return HttpResponse("Hello, Gino!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
